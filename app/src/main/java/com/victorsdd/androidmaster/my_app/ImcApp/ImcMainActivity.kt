104c4117afa68c037e790135365da83b58da111f
victorsdd <>
2025-01-02 20:52:47 -0700

    BMI app has been completed

--- app/src/main/java/com/victorsdd/androidmaster/my_app/ImcApp/ImcMainActivity.kt
+++ app/src/main/java/com/victorsdd/androidmaster/my_app/ImcApp/ImcMainActivity.kt
@@ -1,7 +1,12 @@
 package com.victorsdd.androidmaster.my_app.ImcApp
+
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.Menu
+import android.view.MotionEvent
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.PopupMenu
@@ -12,261 +17,188 @@ import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import com.victorsdd.androidmaster.R
-import java.text.DecimalFormat
-
 
 enum class UnitMeasurement(val unit: String) {
-    M("M"),
-    CM("CM"),
-    FT("FT"),
-    IN("IN"),
+    M("M"), CM("CM"), FT("FT"), IN("IN")
 }
 
-
-enum class WeightMode{
-    INCREASE,
-    DECREASE,
-}
-
-enum class AgeMode{
-    INCREASE,
-    DECREASE,
-}
-
+enum class AdjustmentMode { INCREASE, DECREASE }
 
 class ImcMainActivity : AppCompatActivity() {
+
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
     private lateinit var height: TextView
     private lateinit var slider: RangeSlider
     private lateinit var unit: TextView
     private lateinit var menu: ImageButton
+    private lateinit var increaseWeightButton: FloatingActionButton
+    private lateinit var decreaseWeightButton: FloatingActionButton
+    private lateinit var weight: TextView
+    private lateinit var weightUnitM: TextView
+    private lateinit var tvAge: TextView
+    private lateinit var increaseAgeButton: FloatingActionButton
+    private lateinit var decreaseAgeButton: FloatingActionButton
+    private lateinit var calculateBMI: Button
 
     private var unitMeasurement: UnitMeasurement = UnitMeasurement.M
-
-    private var isMaleSelected: Boolean = true
-
-    private lateinit var increaseWeightButton : FloatingActionButton
-
-    private lateinit var decreaseWeightButton : FloatingActionButton
-
-    private lateinit var weight : TextView
-
-    private lateinit var weightUnitM : TextView
-
-    private lateinit var tvAge: TextView
-
-    private lateinit var increaseAgeButton : FloatingActionButton
-
-    private lateinit var decreaseAgeButton : FloatingActionButton
-
-    private var currentWeight: Int = 0
-    private var currentAge: Int = 0
-    private var currentHeight: Int = 0
-
-    private lateinit var calculateBMI : Button
-
+    private var isMaleSelected = true
+    private var currentWeight = 0
+    private var currentAge = 0
+    private var currentHeight = 120
 
     companion object {
         const val IMC_KEY = "IMC_RESULT"
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_main)
         initComponents()
         initListeners()
-
     }
 
-    private fun initComponents(){
+    private fun initComponents() {
         viewMale = findViewById(R.id.view_male)
         viewFemale = findViewById(R.id.view_female)
         menu = findViewById(R.id.btn_popup_menu)
         unit = findViewById(R.id.tv_unitMeasurement)
-        unit.text = unitMeasurement.unit
-
         height = findViewById(R.id.tv_height)
         slider = findViewById(R.id.slider)
-
         increaseWeightButton = findViewById(R.id.btn_increase)
         decreaseWeightButton = findViewById(R.id.btn_decrease)
         weight = findViewById(R.id.textview_weight)
-
         weightUnitM = findViewById(R.id.weight_unitM)
-
         tvAge = findViewById(R.id.tv_age)
-
         increaseAgeButton = findViewById(R.id.btn_increase_age)
         decreaseAgeButton = findViewById(R.id.btn_decrease_age)
-
-        calculateBMI = findViewById(R.id.btn_calculatebmi)
-
-
+        calculateBMI = findViewById(R.id.btn_calculate)
     }
 
-    private fun initListeners(){
-        viewMale.setOnClickListener {
-            changeGender()
-            setGenderColor()
-        }
-        viewFemale.setOnClickListener {
-            changeGender()
-            setGenderColor()
-        }
-        menu.setOnClickListener {
-            showMenu()
-        }
-
-        slider.addOnChangeListener { _, value, _ ->
-            setHeight(value)
-        }
-
-        increaseWeightButton.setOnClickListener{
-            increaseOrDecreaseWeight(WeightMode.INCREASE)
-        }
-
-        decreaseWeightButton.setOnClickListener{
-            increaseOrDecreaseWeight(WeightMode.DECREASE)
-        }
-
-        increaseAgeButton.setOnClickListener{
-            increaseOrDecreaseAge(AgeMode.INCREASE)
-        }
-
-        decreaseAgeButton.setOnClickListener{
-            increaseOrDecreaseAge(AgeMode.DECREASE)
-        }
-
-        calculateBMI.setOnClickListener{
-            navigateToResult(calculateBMI())
-        }
-
-
-    }
-
-    private fun navigateToResult(result: Double){
-        val intent = Intent(this, ResultImcActivity::class.java)
-        intent.putExtra(IMC_KEY, result)
-        startActivity(intent)
-    }
-
-    private fun calculateBMI() : Double{
-        val df = DecimalFormat("#.##")
-        val imc = currentWeight / (currentHeight.toDouble() / 100  * currentHeight.toDouble() / 100)
-        return df.format(imc).toDouble()
-    }
-
-    private fun increaseOrDecreaseWeight(mode: WeightMode) {
-        when (mode) {
-            WeightMode.INCREASE -> {
-                weight.text = (weight.text.toString().toInt() + 1).toString()
-                currentWeight = weight.text.toString().toInt()
-            }
-            WeightMode.DECREASE -> {
-                if(weight.text.toString().toInt() > 1){
-                    weight.text = (weight.text.toString().toInt() - 1).toString()
-                    currentWeight = weight.text.toString().toInt()
-                }
-
-            }
-            else -> println("Invalid option")
+    private val handler = Handler(Looper.getMainLooper())
+    private val incrementRunnable = object : Runnable {
+        override fun run() {
+            currentWeight = adjustValue(currentWeight, AdjustmentMode.INCREASE, 1)
+            weight.text = currentWeight.toString()
+            handler.postDelayed(this, 100)
         }
     }
 
-    private fun increaseOrDecreaseAge(mode: AgeMode){
-        when(mode){
-            AgeMode.INCREASE -> {
-                tvAge.text = (tvAge.text.toString().toInt() + 1).toString()
-                currentAge = tvAge.text.toString().toInt()
-            }
-            AgeMode.DECREASE -> {
-                if(tvAge.text.toString().toInt() > 5) {
-                    tvAge.text = (tvAge.text.toString().toInt() - 1).toString()
-                    currentAge = tvAge.text.toString().toInt()
-                }
-            }
-            else -> println("Invalid option")
+    private val decrementRunnable = object : Runnable {
+        override fun run() {
+            currentWeight = adjustValue(currentWeight, AdjustmentMode.DECREASE, 1)
+            weight.text = currentWeight.toString()
+            handler.postDelayed(this, 50)
         }
     }
 
-    private fun setHeight(value: Float){
-        when(this.unitMeasurement){
-            UnitMeasurement.M -> {
-                val formattedValue = String.format("%.0f", value).split("")
-                var completedValue : String = "${formattedValue[1]}.${formattedValue[2]}${formattedValue[3]}"
-                height.text = "$completedValue"
-            }
-            UnitMeasurement.CM -> {
-                var formattedValue = String.format("%.0f", value)
-                height.text = "$formattedValue"
-            }
-            UnitMeasurement.FT -> {
-                val formattedValue = String.format("%.2f", value / 30.48)
-                height.text = "$formattedValue"
-            }
-            UnitMeasurement.IN -> {
-                val formattedValue = String.format("%.2f", value / 2.54)
-                height.text = "$formattedValue"
-            }
-        }
-    }
+    private fun initListeners() {
+        viewMale.setOnClickListener { toggleGender(true) }
+        viewFemale.setOnClickListener { toggleGender(false) }
+        menu.setOnClickListener { showMenu() }
+        slider.addOnChangeListener { _, value, _ -> updateHeight(value) }
+        increaseAgeButton.setOnClickListener { adjustAge(AdjustmentMode.INCREASE) }
+        decreaseAgeButton.setOnClickListener { adjustAge(AdjustmentMode.DECREASE) }
+        calculateBMI.setOnClickListener { calculateBMI() }
 
-    private fun setGenderColor() {
-        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
-        viewFemale.setCardBackgroundColor(getBackgroundColor(!isMaleSelected))
-    }
 
-    private fun getBackgroundColor(isSelected: Boolean): Int {
-        val colorReference = if (isSelected) {
-            R.color.background_component_selected
-        } else {
-            R.color.background_component
-        }
-
-        return ContextCompat.getColor(this, colorReference)
-    }
-
-    private fun changeGender(){
-        isMaleSelected = !isMaleSelected
-    }
-
-    private fun showMenu(){
-        val popupMenu = PopupMenu(this, menu)
-        popupMenu.inflate(R.menu.popup_menu)
-        val menu = popupMenu.menu
-        for (unit in UnitMeasurement.values()){
-            menu.add(Menu.NONE, unit.ordinal, Menu.NONE, unit.unit)
-        }
-        popupMenu.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                UnitMeasurement.M.ordinal -> {
-                    setUnitMeasurement(UnitMeasurement.M)
+        increaseWeightButton.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    handler.post(incrementRunnable)
                     true
                 }
-                UnitMeasurement.CM.ordinal -> {
-                    setUnitMeasurement(UnitMeasurement.CM)
-                    true
-                }
-                UnitMeasurement.FT.ordinal -> {
-                    setUnitMeasurement(UnitMeasurement.FT)
-                    true
-                }
-                UnitMeasurement.IN.ordinal -> {
-                    setUnitMeasurement(UnitMeasurement.IN)
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(incrementRunnable)
                     true
                 }
                 else -> false
             }
         }
 
+        decreaseWeightButton.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    handler.post(decrementRunnable)
+                    true
+                }
+
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    handler.removeCallbacks(decrementRunnable)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun toggleGender(isMale: Boolean) {
+        isMaleSelected = isMale
+        updateGenderColors()
+    }
+
+    private fun updateGenderColors() {
+        viewMale.setCardBackgroundColor(getGenderColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getGenderColor(!isMaleSelected))
+    }
+
+    private fun getGenderColor(isSelected: Boolean): Int {
+        val colorRes = if (isSelected) R.color.background_component_selected else R.color.background_component
+        return ContextCompat.getColor(this, colorRes)
+    }
+
+    private fun updateHeight(value: Float) {
+        currentHeight = when (unitMeasurement) {
+            UnitMeasurement.M -> value.toInt()
+            UnitMeasurement.CM -> value.toInt()
+            UnitMeasurement.FT -> (value / 30.48).toInt()
+            UnitMeasurement.IN -> (value / 2.54).toInt()
+        }
+        height.text = currentHeight.toString()
+    }
+
+    private fun adjustWeight(mode: AdjustmentMode) {
+        currentWeight = adjustValue(currentWeight, mode, 1)
+        weight.text = currentWeight.toString()
+    }
+
+    private fun adjustAge(mode: AdjustmentMode) {
+        currentAge = adjustValue(currentAge, mode, 5)
+        tvAge.text = currentAge.toString()
+    }
+
+    private fun adjustValue(value: Int, mode: AdjustmentMode, min: Int): Int {
+        return when (mode) {
+            AdjustmentMode.INCREASE -> value + 1
+            AdjustmentMode.DECREASE -> if (value > min) value - 1 else value
+        }
+    }
+
+    private fun calculateBMI() {
+        val heightInMeters = currentHeight / 100.0
+        val bmi = currentWeight / (heightInMeters * heightInMeters)
+        navigateToResult(bmi)
+    }
+
+    private fun navigateToResult(result: Double) {
+        val intent = Intent(this, ResultImcActivity::class.java).apply {
+            putExtra(IMC_KEY, result)
+        }
+        startActivity(intent)
+    }
+
+    private fun showMenu() {
+        val popupMenu = PopupMenu(this, menu).apply {
+            inflate(R.menu.popup_menu)
+            UnitMeasurement.values().forEach { unit ->
+                menu.add(Menu.NONE, unit.ordinal, Menu.NONE, unit.unit)
+            }
+            setOnMenuItemClickListener { item ->
+                unitMeasurement = UnitMeasurement.values()[item.itemId]
+                unit.text = unitMeasurement.unit
+                true
+            }
+        }
         popupMenu.show()
     }
-
-    private fun setUnitMeasurement(unitMeasurement: UnitMeasurement){
-        this.unitMeasurement = unitMeasurement
-        unit.text = unitMeasurement.unit
-    }
-
 }
--- app/src/main/java/com/victorsdd/androidmaster/my_app/ImcApp/ResultImcActivity.kt
+++ app/src/main/java/com/victorsdd/androidmaster/my_app/ImcApp/ResultImcActivity.kt
@@ -1,30 +1,79 @@
 package com.victorsdd.androidmaster.my_app.ImcApp
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import com.victorsdd.androidmaster.R
 import com.victorsdd.androidmaster.my_app.ImcApp.ImcMainActivity.Companion.IMC_KEY
 
 class ResultImcActivity : AppCompatActivity() {
 
-    private lateinit var result: TextView
+    private lateinit var bmiTextView: TextView
+    private lateinit var resultTextView: TextView
+    private lateinit var backButton: ImageView
+    private lateinit var recalculateBtn : Button
+    private lateinit var resultType : TextView
+    private var result: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.result_activity)
-        val result : Double = intent.extras?.getDouble(IMC_KEY) ?: -1.0
-        initialize()
+        result = intent.getDoubleExtra(IMC_KEY, 0.0)
+        initViews()
+        setListeners()
+        displayResult()
+        setResultType()
     }
 
-    private fun initialize(){
+    private fun initViews() {
 
-        //result = findViewById(R.id.tvResult)
-        //result.text = result.toString()
+        backButton = findViewById(R.id.back_button)
+        bmiTextView = findViewById(R.id.result)
+        resultTextView = findViewById(R.id.result_text)
+        resultType = findViewById(R.id.result_type)
+        recalculateBtn = findViewById(R.id.btn_recalculate)
+    }
 
+    private fun setListeners() {
+        backButton.setOnClickListener { navigateBack() }
 
+        recalculateBtn.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun setResultType(){
+
+        when (result) {
+            in 0.0..18.5 -> {
+                resultType.text = getText(R.string.eat_more)
+                resultType.setTextColor(ContextCompat.getColor(this, R.color.peso_bajo))
+                resultTextView.text = getString(R.string.low_weight)
+            }
+            in 18.5..24.9 -> {
+                resultType.text = getText(R.string.normal)
+                resultType.setTextColor(ContextCompat.getColor(this, R.color.peso_normal))
+                resultTextView.text = getText(R.string.healthy)
+            }
+            in 25.0..29.9 -> {
+                resultType.text = getText(R.string.high_weight)
+                resultType.setTextColor(ContextCompat.getColor(this, R.color.peso_sobrepeso))
+                resultTextView.text = getText(R.string.over_weight)
+            }
+        }
+    }
+
+    private fun displayResult() {
+        bmiTextView.text = String.format("%.2f", result)
+    }
+
+    private fun navigateBack() {
+        Intent(this, ImcMainActivity::class.java).also { startActivity(it) }
     }
 }
